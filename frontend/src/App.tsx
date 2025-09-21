85d89c56813e7be6f71d18e209033bedb6b45d93
jiakai-17 <111171022+jiakai-17@users.noreply.github.com>
2025-09-21 19:00:29 +0800

    feat: add tailwindcss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
 
 const App = () => {
   return (
     <div className="content">
       <h1>Rsbuild with React</h1>
+      <h1 className="text-3xl font-bold underline">Hello Tailwind!</h1>
       <p>Start building amazing things with Rsbuild.</p>
     </div>
   );
