87868ea66eb843a98a54ac2a31b01b99409fbec8
jiakai-17 <111171022+jiakai-17@users.noreply.github.com>
2025-09-22 18:56:05 +0800

    chore: add remote font

--- frontend/rsbuild.config.ts
+++ frontend/rsbuild.config.ts
@@ -18,4 +18,21 @@ export default defineConfig({
       ],
     },
   },
+  html: {
+    tags: [
+      {
+        tag: "link",
+        attrs: {
+          href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+          rel: "stylesheet",
+        },
+      },
+    ]
+  },
+  performance: {
+    preconnect: [
+      { href: "https://fonts.googleapis.com" },
+      { href: "https://fonts.gstatic.com", crossorigin: true },
+    ],
+  }
 });
