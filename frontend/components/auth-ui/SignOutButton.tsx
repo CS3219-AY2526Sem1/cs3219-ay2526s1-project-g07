4046e839ad430fe93ad934ca6ede1bdf201a1a24
tanshiyu <tanshiyu1999@gmail.com>
2025-09-22 09:37:41 +0800

    remove comment

--- frontend/components/auth-ui/SignOutButton.tsx
+++ frontend/components/auth-ui/SignOutButton.tsx
@@ -1,16 +1,12 @@
 
 import { useEffect, useState } from "react";
-// import { useNavigate } from "@tanstack/react-router";
 import { signOut, useSession } from "../../lib/auth-client";
 import { Button } from "../ui/button";
 
 export default function SignOutButton() {
-  // const [loading, setLoading] = useState(false);
-  // const [error, setError] = useState<string | null>(null);
-  // const navigate = useNavigate();
+
 
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
-  // const [loading, setLoading] = useState<boolean>(true);
 
   const session = useSession()
 
