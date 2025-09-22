03241f27a69b1f69644019c1bfc7a1ebcf7acd8d
jiakai-17 <111171022+jiakai-17@users.noreply.github.com>
2025-09-22 14:09:39 +0800

    feat: add tanstack router

--- frontend/rsbuild.config.ts
+++ frontend/rsbuild.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
+import { tanstackRouter } from "@tanstack/router-plugin/rspack";
 
 export default defineConfig({
   plugins: [pluginReact()],
   server: {
     host: "127.0.0.1",
     port: 3000,
+  },
+  tools: {
+    rspack: {
+      plugins: [
+        tanstackRouter({
+          target: "react",
+          autoCodeSplitting: true,
+        }),
+      ],
+    },
   },
 });
