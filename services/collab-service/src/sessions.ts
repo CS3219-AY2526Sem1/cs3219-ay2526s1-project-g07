8d32cb016c677b516725ca6b25250f569e097f1f
Sebastian Tay <taysebastian95@gmail.com>
2025-10-26 14:58:48 +0800

    Update fields in sessions in-memory store for collabservice

--- services/collab-service/src/sessions.ts
+++ services/collab-service/src/sessions.ts
@@ -1,9 +1,18 @@
 // In-memory store of authorised rooms and their matched users (FROM KAFKA)
-const sessions = new Map();
-sessions.set("dummy-session-id", new Set(["user1", "user2"])); // Example session
+const sessions: Map<string, Map<string, string>> = new Map();
+sessions.set("dummy-session-id", 
+  new Map([
+    ["user1", "user1"], 
+    ["user2", "user2"],
+    ["questionId", "dummy-question-id"],
+    ["title", "dummy-title"],
+    ["question", "dummy-question"],
+    ["difficulty", "dummy-difficulty"],
+    ["categories", "dummy-category"],
+  ])); //Example
 
-export const addSession = (sessionId: string, userIds: string[]) => {
-  sessions.set(sessionId, new Set(userIds));
+export const addSession = (sessionId: string, sessionDetails: Map<string, string>) => {
+  sessions.set(sessionId, sessionDetails);
 };
 
 export const removeSession = (sessionId: string) => {
@@ -15,5 +24,5 @@ export const checkSessionAndUsers = (sessionId: string, userId: string) => {
   if (!session) {
     return false;
   }
-  return session.has(userId);
+  return session?.get("user1") === userId || session.get("user2") === userId;
 };
