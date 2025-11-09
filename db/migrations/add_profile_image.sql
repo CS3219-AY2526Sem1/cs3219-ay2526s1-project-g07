f61486a091357ff58db2f5b9225ae4738ec6c378
Tan Shi Yu <tanshiyu1999@gmail.com>
2025-11-10 01:56:31 +0800

    migrate the session with collab_id

--- db/migrations/add_profile_image.sql
+++
@@ -1,8 +0,0 @@
--- Migration: Add profileImage column to user table
--- Date: 2025-11-10
-
-ALTER TABLE "user" 
-ADD COLUMN IF NOT EXISTS "profileImage" TEXT;
-
--- Add comment for documentation
-COMMENT ON COLUMN "user"."profileImage" IS 'Compressed profile image stored as base64 string';
