44f04c4af93a7a488129848fc3de541737411265
Sebastian Tay <taysebastian95@gmail.com>
2025-11-11 22:56:05 +0800

    Fix based on suggestions in review

--- services/ai-service/src/routes/index.ts
+++ services/ai-service/src/routes/index.ts
@@ -33,7 +33,7 @@ app.post("/hint", async (c) => {
 
   console.log(`Received hint request from ${userId} in ${collabSessionId}`);
 
-  let response = null;
+  let response: { collabSessionId: string, userId: string, question: string } | null = null;
   try {
     response = await kafkaClient.retrieveQuestionDetails(collabSessionId, userId);
 
